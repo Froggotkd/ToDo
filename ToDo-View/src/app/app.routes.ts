b8ecb646b1a376f40838f0ba48f03c3bf39c5cd5
Froggotkd <djojavi19@gmail.com>
2025-08-11 14:24:21 -0500

    Feature routing create task

--- ToDo-View/src/app/app.routes.ts
+++ ToDo-View/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { ViewTasksComponent } from './tasks/view-tasks/view-tasks.component';
 import { LoginComponent } from './login/login.component';
 import { authGuard } from './core/auth.guard';
 
 export const routes: Routes = [
     {path: '',canActivate:[authGuard], loadComponent: () => import('./tasks/view-tasks/view-tasks.component').then(m => m.ViewTasksComponent)  },
+    {path: 'create-task',canActivate:[authGuard], loadComponent: () => import('./tasks/create-task/create-task.component').then(m => m.CreateTaskComponent) },
     {path: 'login', component: LoginComponent}
 ];
--- ToDo-View/src/app/tasks/view-tasks/view-tasks.component.ts
+++ ToDo-View/src/app/tasks/view-tasks/view-tasks.component.ts
@@ -5,11 +5,12 @@ import { TaskCardComponent } from "../task-card/task-card.component";
 import { MatCard } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { trigger, transition, style, animate } from '@angular/animations';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-view-tasks',
-  imports: [TaskCardComponent, MatCard, MatIconModule],
+  imports: [TaskCardComponent, MatCard, MatIconModule, RouterLink],
   templateUrl: './view-tasks.component.html',
   styleUrls: ['./view-tasks.component.css'],
   providers: [TasksService],
@@ -40,5 +41,7 @@ export class ViewTasksComponent implements OnInit {
       error: (err) => console.error('Error al cargar tareas', err)
     });
   }
+
+
 }
 
